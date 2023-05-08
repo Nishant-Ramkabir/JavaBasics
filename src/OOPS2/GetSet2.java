3c5a189fd2f6cb2624f7100e2728677a7d61b623
Nishant Ramkabir <nishant.ramkabir@CDAHMLPT004.cignexdatamatics.com>
2023-05-08 14:15:56 +0530

    Update08/05

---
+++ src/OOPS2/GetSet2.java
@@ -0,0 +1,17 @@
+package OOPS2;
+
+public class GetSet2{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		GetSet a = new GetSet();
+		
+		a.setName("Harshi");
+		
+		System.out.println("Name "+a.getName());
+		
+		
+	}
+
+}
--- src/OOPS2/TestCar.java
+++ src/OOPS2/TestCar.java
@@ -13,12 +13,32 @@ public class TestCar {
 		c.start();
 		
 		
-		//Top casting
+		//Top/Up casting
 		//Runtime polymorphism
 		Car c1= new Bmw();
+		
 		//it will call the overridden method
 	
 		c1.start();
+		System.out.println("/////////////////////");
+		//Down Casting
+/*		try {
+			Car c2= new Car();
+			Bmw b2= (Bmw) c2;
+			b2.start();
+			
+		} catch (ClassCastException e) {
+
+			System.out.println(e);
+		}
+*/		
+
+		
+		Car c2 = new Bmw ();
+		Bmw b2 = (Bmw) c2;
+		b2.start();
+		
+		
 	}
 
 }
